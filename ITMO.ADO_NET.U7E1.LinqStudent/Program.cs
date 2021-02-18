216324b675c1a942d132a8c0d03b94d02adfb243
AlexanderSolomin <a.solomin@ymail.com>
2021-02-18 11:56:11 +0300

    u7e1

---
+++ ITMO.ADO_NET.U7E1.LinqStudent/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.ADO_NET.U7E1.LinqStudent
+{
+    class Program
+    {
+        static List<Student> students = new List<Student>
+            {
+                new Student {First="Svetlana", Last="Omelchenko", ID=111,
+                    Scores = new List<int> {96, 92, 81, 60}},
+                new Student {First="Claire", Last="O’Donnell", ID=112,
+                    Scores = new List<int> {75, 84, 91, 39}},
+                new Student {First="Sven", Last="Mortensen", ID=113,
+                    Scores = new List<int> {88, 94, 65, 91}},
+                new Student {First="Cesar", Last="Garcia", ID=114,
+                    Scores=new List<int> {97, 89, 85, 82}},
+                new Student {First="Debra", Last="Garcia", ID=115,
+                    Scores=new List<int> {35, 72, 91, 70}},
+            };
+        static void Main(string[] args)
+        {
+            IEnumerable<Student> studentQuery =
+                from student in students
+                where student.Scores[0] > 90 && student.Scores[3] < 80
+                select student;
+            var studentQueryW = students.Where(st => st.Scores[0] > 90 && st.Scores[3] < 80);
+
+            foreach (Student student in studentQueryW)
+            {
+                Console.WriteLine($"{nameof(studentQueryW)} {student.Last}, {student.First}");
+            }
+
+            int studentCount = (
+                from student in students
+                where student.Scores[0] > 90 && student.Scores[3] < 80
+                select student).Count();
+
+            int studentCountW = students.Where(st => st.Scores[0] > 90 && st.Scores[3] < 80).Count();
+
+            Console.WriteLine($"{nameof(studentCountW)} {studentCount}, {studentCountW}");
+
+            var studentList = (
+                from student in students
+                where student.Scores[0] > 90 //&& student.Scores[3] < 80
+                orderby student.Scores[0] descending
+                select student).ToList();
+            foreach (Student student in studentList)
+            {
+                Console.WriteLine($"{nameof(studentList)} {student.Last}, {student.First} sc {student.Scores[0]}");
+            }
+
+        }
+    }
+}
